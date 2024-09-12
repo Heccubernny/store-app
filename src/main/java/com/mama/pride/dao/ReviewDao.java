b0efa6993361daaae9c274755f6f346a9b35b7ce
Ikubanni Paul <ipom4eva@gmail.com>
2024-09-12 21:43:10 +0100

    deploying to azure app service

--- src/main/java/com/mama/pride/dao/ReviewDao.java
+++ src/main/java/com/mama/pride/dao/ReviewDao.java
@@ -7,7 +7,6 @@ import com.mama.pride.entities.Review;
 public interface ReviewDao extends JpaRepository<Review, Integer> {
     // @Query("SELECT AVG(star) FROM Review WHERE product_id=(:productId) GROUP BY
     // product_id")
-
     // Double getAverageStar(@Param("productId") int productId);
 
     @SuppressWarnings({ "unchecked", "null" })
